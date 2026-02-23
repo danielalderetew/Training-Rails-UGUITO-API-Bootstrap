ac14b97a0dcda688c0fd6ef29e9bb1ae78b42cb7
Daniel Alderete <daniel.alderete@widergy.com>
2026-02-23 10:38:11 -0300

    fix: fixes

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -28,6 +28,16 @@ module Api
             .page(notes_params[:page])
             .per(notes_params[:page_size] || 10)
       end
+
+      def pagination_meta(collection)
+        { 
+          current_page: collection.current_page,
+          next_page: collection.next_page,
+          prev_page: collection.prev_page,
+          total_pages: collection.total_pages,
+          total_count: collection.total_count 
+        }
+      end
     end
   end
 end
