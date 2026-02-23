bee756fd1c902f8d977e14e3d6bbbe16729c5ce5
Daniel Alderete <daniel.alderete@widergy.com>
2026-02-22 22:48:37 -0300

    feat: test

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,8 +1,36 @@
 FactoryBot.define do
   factory :note do
-    title { "MyString" }
-    content { "MyString" }
-    note_type { "MyString" }
-    user { nil }
+    title { Faker::Book.title }
+    content { Faker::Lorem.words(number: 50).join(" ") }
+    note_type { :critique }
+    user
+
+    trait :review do
+      note_type { :review }
+    end
+
+    trait :critique do
+      note_type { :critique }
+    end
+
+    trait :long_content do
+      content { Faker::Lorem.words(number: 150).join(" ") }
+    end
+
+    trait :medium_content do
+      content { Faker::Lorem.words(number: 50).join(" ") }
+    end
+
+    trait :short_content do
+      content { Faker::Lorem.words(number: 10).join(" ") }
+    end
+
+    trait :north do
+      association :user, factory: [:user, :north]
+    end
+
+    trait :south do
+      association :user, factory: [:user, :south]
+    end
   end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:note) { build(:note, note_type_trait, content_trait, user_trait) }
+
+  let(:note_type_trait) { :critique }
+  let(:content_trait) { :short_content }
+  let(:user_trait) { :north }
+
+  shared_examples 'a valid note' do |content_trait_value|
+      let(:content_trait) { content_trait_value }
+      it { is_expected.to be_valid }
+  end
+  
+  shared_examples 'an invalid note' do |content_trait_value|
+      let(:content_trait) { content_trait_value }
+      it { is_expected.not_to be_valid }
+  end
+
+  %i[title content note_type].each do |value|
+    it { is_expected.to validate_presence_of(value) }
+  end
+  
+
+  it { is_expected.to belong_to(:user) }
+  
+  it { is_expected.to define_enum_for(:note_type).with_values(critique: 0, review: 1) }
+
+
+  it 'has a valid factory' do
+    expect(subject).to be_valid
+  end
+
+  describe '#word_count' do
+    it 'returns the word count of the content' do
+      expect(subject.word_count).to eq(subject.content.split.count)
+    end
+  end
+
+  describe '#validate_review_content' do
+    context 'when user is North' do
+    let(:user_trait) { :north }
+
+    context 'and note is a review' do
+      let(:note_type_trait) { :review }
+
+      it_behaves_like 'a valid note', :short_content
+      it_behaves_like 'a valid note', :medium_content
+      it_behaves_like 'an invalid note', :long_content
+    end
+
+    context 'and note is a critique' do
+      let(:note_type_trait) { :critique }
+
+      it_behaves_like 'a valid note', :short_content
+      it_behaves_like 'a valid note', :medium_content
+      it_behaves_like 'a valid note', :long_content
+    end
+  end
+
+  context 'when user is South' do
+    let(:user_trait) { :south }
+
+    context 'and note is a review' do
+      let(:note_type_trait) { :review }
+
+      it_behaves_like 'a valid note', :short_content
+      it_behaves_like 'a valid note', :medium_content
+      it_behaves_like 'an invalid note', :long_content
+    end
+
+    context 'and note is a critique' do
+      let(:note_type_trait) { :critique }
+
+      it_behaves_like 'a valid note', :short_content
+      it_behaves_like 'a valid note', :medium_content
+      it_behaves_like 'a valid note', :long_content
+    end
+  end
+    
+
+  end
+
 end
