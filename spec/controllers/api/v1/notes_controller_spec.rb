0cea0c5b040b6e5102f24a99942c30f9412e1fec
Daniel Alderete <daniel.alderete@widergy.com>
2026-02-26 16:28:58 -0300

    feat: show test

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe "Notes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/notes/index"
-      expect(response).to have_http_status(:success)
+describe Api::V1::NotesController, type: :controller do
+
+  describe 'GET #show' do
+    let(:note) { create(:note, :short_content, user: user) }
+    
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
+
+      let(:expected) { ShowNoteSerializer.new(note, root: false).to_json }
+
+      context 'when fetching a valid note' do
+
+        before { get :show, params: { id: note.id } }
+
+        it 'responds with the note json' do
+          expect(response.body).to eq(expected)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'when fetching a invalid note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it 'responds with 404 status' do
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+
+    context 'when there is not a user logged in' do
+      context 'when fetching an note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it_behaves_like 'unauthorized'
+      end
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/notes/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
