c33b44099148cb87f28b9063959462e45355fc10
Daniel Alderete <daniel.alderete@widergy.com>
2026-03-02 12:39:40 -0300

    feat: add response_mapper

--- app/services/utility_service/north/response_mapper.rb
+++ app/services/utility_service/north/response_mapper.rb
@@ -5,6 +5,10 @@ module UtilityService
         { books: map_books(response_body['libros']) }
       end
 
+      def retrieve_notes(_response_code, response_body)
+        { notes: map_notes(response_body['notas']) }
+      end
+
       private
 
       def map_books(books)
@@ -17,6 +21,37 @@ module UtilityService
             image_url: book['imagen_url'],
             publisher: book['editorial'],
             year: book['año']
+          }
+        end
+      end
+
+      def map_user_notes(autor)
+          {
+           email: autor['datos_de_contacto']['email'],
+           first_name: autor['datos_personales']['nombre'],
+           last_name: autor['datos_personales']['apellido']
+          }
+        end
+      end
+
+      def map_book_notes(book)
+          {
+           title: book['titulo'],
+           author: book['autor'],
+           genre: book['genero'],
+          }
+        end
+      end
+      
+      def map_notes(notes)
+        notes.map do |note|
+          {
+            title: note['titulo'],
+            type: note['tipo'],
+            created_at: note['fecha_creacion'],
+            content: note['contenido'],
+            user: map_user_notes(note['autor']),
+            book: map_book_notes(note['libro'])
           }
         end
       end
